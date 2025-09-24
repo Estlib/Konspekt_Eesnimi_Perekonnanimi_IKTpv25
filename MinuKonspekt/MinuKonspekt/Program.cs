048c53b33d19ff86b3f0fe9989ebd7acaaae9997
Estlib <75094683+Estlib@users.noreply.github.com>
2025-09-24 10:53:49 +0300

    lisasin kalkulaatori

--- MinuKonspekt/MinuKonspekt/Program.cs
+++ MinuKonspekt/MinuKonspekt/Program.cs
@@ -84,6 +84,49 @@
             {
                 Console.WriteLine("pikkust ei tunta");
             }
+
+            //3 kalkulaator ifi ja elsifidega
+            Console.WriteLine("Tere. Sisesta esimene arv");
+            int arv1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Tere. Sisesta teine arv");
+            int arv2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Sisesta tehtemärk: / * + -");
+            string tehtetyyp = Console.ReadLine();
+
+            int tulemus = 0;
+            if (tehtetyyp == "+")
+            {
+                tulemus = arv1 + arv2;
+            }
+            else if (tehtetyyp == "-")
+            {
+                tulemus = arv1 - arv2;
+            }
+            else if (tehtetyyp == "/")
+            {
+                tulemus = arv1 / arv2;
+            }
+            else if (tehtetyyp == "*")
+            {
+                tulemus = arv1 * arv2;
+            }
+            else if (tehtetyyp == "*")
+            {
+                tulemus = (int)Math.Pow(arv1, arv2);
+            }
+            else
+            {
+                Console.WriteLine("Palun sisesta tehe, mida kalkulaator tuvastada oskab");
+            }
+
+            if (tulemus != 0)
+            {
+                Console.WriteLine(tulemus);
+            }
+            else
+            {
+                Console.WriteLine("Tulemus on 0, või üritasid teha tehet, mida kalkulaator ei tunne");
+            }
         }
     }
 }
