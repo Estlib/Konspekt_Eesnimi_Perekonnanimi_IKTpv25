1ee5a8bcb44081656893d98d5330f792926636b8
Estlib <75094683+Estlib@users.noreply.github.com>
2025-09-24 10:42:07 +0300

    lisasin else if double näite

--- MinuKonspekt/MinuKonspekt/Program.cs
+++ MinuKonspekt/MinuKonspekt/Program.cs
@@ -53,6 +53,37 @@
             {
                 Console.WriteLine("Saad osta Monsterit, YIPPIE ✨✨");
             }
+            //2.3 mitu vahemikku, if-elseif abil, double/float
+            Console.WriteLine("Sisesta oma pikkus ka, palun, "+nimi+":");
+            double pikkus = double.Parse(Console.ReadLine());
+            if (pikkus < 1.00d)
+            {
+                Console.WriteLine("Oled juntsu");
+            }
+            else if (pikkus <1.25d && pikkus > 1.00d)
+            {
+                Console.WriteLine("Oled peaaegu allameetrimees");
+            }
+            else if (pikkus <1.50d && pikkus >1.25d)
+            {
+                Console.WriteLine("Oioi, päkapikk enam ei olegi "+nimi+" !");
+            }
+            else if (pikkus <1.75d && pikkus >1.5d)
+            {
+                Console.WriteLine("Oled enamasti standardpikkuses, kui mitte just natuke lühike");
+            }
+            else if (pikkus <2.00d && pikkus >1.75d)
+            {
+                Console.WriteLine("Pikk kolge, vaata et sa pead vastu uksepiita ära ei löö.");
+            }
+            else if (pikkus > 2.00d)
+            {
+                Console.WriteLine("Täielik tulnukas, kuidas pilved välja näevad?");
+            }
+            else
+            {
+                Console.WriteLine("pikkust ei tunta");
+            }
         }
     }
 }
