23141df3d7f90420741fc58e9687b82ad2501dbb
Estlib <75094683+Estlib@users.noreply.github.com>
2025-10-17 15:21:26 +0300

    allahindluse kalkulaatori näidis

--- MinuKonspekt/MinuKonspekt/Program.cs
+++ MinuKonspekt/MinuKonspekt/Program.cs
@@ -415,6 +415,29 @@
             ////tehte tulemus. Selle jaoks on WriteLine funktsioonis parameetrina pandud muutuja
             ////"tulemus" ilma tekstiks teisendamata. Lause lõppeb lauselõpumärgiga ";".
 
+            Console.WriteLine("Sisesta ostusumma");
+            double ostusumma = double.Parse(Console.ReadLine());
+            if (ostusumma > 100)
+            {
+                Console.WriteLine("Saad 20% allahindlust!!!!!!!!!!!!!!!OMFGZEROONE!!!!!!!!!!!!!!!!!!");
+            }
+            else if (ostusumma < 101 && ostusumma > 50)
+            {
+                Console.WriteLine("Saad 10% allahindlust. c: yay");
+            }
+            else if (ostusumma < 51 && ostusumma > 20)
+            {
+                Console.WriteLine("5% allahindlust.");
+            }
+            else if (ostusumma < 21 && ostusumma > 0)
+            {
+                Console.WriteLine("allahindlust ei saa");
+            }
+            else
+            {
+                Console.WriteLine("sisestatud on vigane arv");
+            }
+
             //string kasutajaNimi = "";
             //do
             //{
@@ -458,7 +481,7 @@
             else if (true) { } //kaitstud sõnad "else" ja "if" (else if) kutsuvad esile sekundaarse tingimuslause, mille tingimus
                                //on samamoodi sulgude vahel, ning millele pepab eelnema alat kas "if" või teine "else if". Tingimuse täitumisel
                                //ja eelneva tingimuse mittetäitumisel, teostatakse koodiploki sees olev kood.
-            else {} //kaitstud sõna "else" kutsub esile järeltingimuse, millele peab eelnema kas "if" või "else if", ning mille koodiploki sisu
+            else { } //kaitstud sõna "else" kutsub esile järeltingimuse, millele peab eelnema kas "if" või "else if", ning mille koodiploki sisu
                     //täidetakse kõikide teiste "if" ja "else if" tingimuste läbikukkumisel.
 
             /* Loogilised tehted */
