a6f194e4a1948859e017cd4985a144ca8ccc72e5
Estlib <75094683+Estlib@users.noreply.github.com>
2025-10-31 13:55:28 +0200

    for-tsükkel ja massiiv andmetüüp

--- MinuKonspekt/MinuKonspekt/Program.cs
+++ MinuKonspekt/MinuKonspekt/Program.cs
@@ -263,6 +263,28 @@
             var y = 123;   //ta võib omada endas teisi andmetüüpe
             const int z = 9; // konstant-tüüpi muutujaid ei saa muuta, nende sisu on read-only
 
+            /* Võimalikud komposiitandmetüübid */
+            // 1. massiiv:
+            // [] - > Massiiv on komposiitandmetüüp, mille sees saab olla mitmeid samat tüüpi lihtandmeid. Massiivi tähistatakse kantsulgudega.
+            //        Massive saab olla ükskõik millist lihtandmetüüpi massiive.
+            //        Massiivi tekitamisel tuleb ära öelda kui pikk või kui suur see massiiv on.
+            //        Massiiv ei pea olema koostatud ainult lihtandmetüüpidest, vaid massiive saab olla ka tehtud teistest komposiitandmetüüpidest
+            //        Sealhulgas massiiv ise.
+            // Esimene tekitusviis:
+            int[] arvuMassiiv = new int[3]; // andmetüüp int väljendab et tegu on täisarvutüüpi andmega ja kantsulud väljendavad et ühtlasi on
+                                            // tegu ka massiiviga. muutuja nimeks on "arvuMassiiv" ja võrdusmärgi abil on omistatud muutujasse
+                                            // uus tühi massiiv kasutades kaitstud sõna "new", millele järgneb selle massiivi andmetüübi ja
+                                            // pikkuse sätestus "int[3]". See täghendab et siin massiivis on 3 elementi, mis on täisarvud.
+            // Teine tekitusviis:
+            int[] arvuMassiiv2 = [1,2,3];   // teine massiivi tekitusviis kus järjendi pikkuse sätestamise asemel, pannakse elemendid kohe
+                                            // järjendit omava muutuja sisse, järjendi pikkust sätestama ei pea, kuna pikkuse tuletab kompilaator
+                                            // sinna sisse pandud elementide koguse järgi.
+
+            // -- massiivi sisemised meetodid:
+            int hasThisMany = arvuMassiiv.Length; // massiivi meetod "Length" mille me saame kasutusele võtta punkti abil, loendab kokku
+                                                  // mitu elementi, adresseeritav massiiv omab, omistatakse ainult järjendi pikkus, mitte
+                                                  // järjendi sees olevaid elemente.
+
             //põhilised matemaatilised tehted
             int liitmine = 1 + 1; //liitmine, kaks arvu liidetakse kokku
             int lahutamine = 1 - 1; //lahutamine, kus esimesest arvust lahutatakse maha teine
@@ -274,7 +296,7 @@
             //muutuja nimed
             int arv = 0; //sobib
             string sõne = "abc"; //sobib
-                                 //string string = "abc"; //kaitstud sõna kasutada ei saa
+            //string string = "abc"; //kaitstud sõna kasutada ei saa
 
             // muutuja nimeks ei sobi järgnevad sõnad:
             //abstract, as, base, bool, break, byte, case,
@@ -566,6 +588,19 @@
             {
                 //koodiplokk kus midagi tehakse
                 i++;      //ning seejärel muudetakse tsüklimuutuja "i" olekut. antud juhul liidetakse 1 juurde kiirtehtega "++".
+            }
+            // 3. for
+            int kogus = 6; // muutuja mida tsükkel kasutab oma töö tegemiseks - teisisõnu, töödeldav materjal
+            for (int k = 0; k < kogus; k++) // kaitstud sõnaa "for" alustab for-tsüklit, pärast mida on sulud, mille vahel on kõik tsükli
+                                             // töö jaoks vajalik olemas. Esimene parameeter, tekitab tsükli töö jaoks kohaliku muutuja
+                                             // "int k = 0;" mida tsükli ENDA töö juhtimiseks. Teine parameeter on tingimuslause, mis kontrollib
+                                             // tingimuse täitumist "k < kogus;" ning mille täitumisel tsükli töö jätkub, aga mille
+                                             // mitte-täitumisel tsükkel katkeb. Kolmas parameeter on tsüklimuutuja inkrementeerimine kiirtehtega
+                                             // "k++". Pane tähele, et iga sulgude vahel oleva osa järel (välja arvatud viimase) on
+                                             // lauselõpumärk. Tsükli tööd kontrolliv tingimuslause koosneb kolmest reast, mitte ühest
+                                             // nagu "while" või "do-while" puhul.
+            {                                // sulgudele järgneb, loogeliste sulgude vahel ole koodiplokk {}
+                Console.WriteLine(k);        // töötlustegevus tsükli sees, on muutuja "k" hetkearvu väljakuvamine.
             }
         }
     }
